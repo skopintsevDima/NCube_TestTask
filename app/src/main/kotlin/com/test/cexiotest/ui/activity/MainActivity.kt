5746c6963fbe4c820752911544adea9226be5278
Dmytro Skopintsev <skopintsev2015@gmail.com>
2020-04-07 17:21:28 +0300

    Basic activity created.

---
+++ app/src/main/kotlin/com/test/cexiotest/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.cexiotest.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.cexiotest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
